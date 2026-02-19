036405e17b1c4a347b71ba726e6b321d31242eea
Kretostan <kubakret82@icloud.com>
2026-02-19 01:07:24 +0100

    fix: remove unnecessary comment (Closes #1, #2)

--- src/index.ts
+++ src/index.ts
@@ -12,5 +12,4 @@ client.once(Events.ClientReady, (readyClient) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// Log in to Discord with your client's token
 client.login(token);
