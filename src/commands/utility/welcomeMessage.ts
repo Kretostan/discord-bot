08e830fec89a68a0bee780bf0a933a0d4a5f1885
Kretostan <kubakret82@icloud.com>
2026-02-26 00:30:21 +0100

    fix: check for administrator permissions (Closes #4)

--- src/commands/utility/welcomeMessage.ts
+++ src/commands/utility/welcomeMessage.ts
@@ -1,14 +1,16 @@
-import { SlashCommandBuilder, MessageFlags, type Interaction } from "discord.js";
+import { SlashCommandBuilder, MessageFlags, PermissionFlagsBits, type Interaction } from "discord.js";
 import { setWelcomeMessage } from "../../db/guildSettings.js";
 
 export default {
     data: new SlashCommandBuilder()
         .setName("member-add-message")
         .setDescription("Set welcome message when member join guild.")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .setDMPermission(false)
         .addStringOption(option => option.setName("message").setDescription("Member add welcome message")),
     async execute(interaction: Interaction) {
-        if (!interaction.isChatInputCommand()) return;
-        if (!interaction.guildId || !interaction.options) return;
+        if (!interaction.isChatInputCommand() || !interaction.guildId) return;
+        if (!interaction || !interaction.memberPermissions || !interaction.memberPermissions.has(PermissionFlagsBits.Administrator)) return;
         
         const message = interaction.options.getString("message", true);
         setWelcomeMessage(interaction.guildId, message);
