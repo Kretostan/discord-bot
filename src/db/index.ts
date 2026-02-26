22a414a23a3d294006f3d95c21713a9c4a5c86f0
Kretostan <kubakret82@icloud.com>
2026-02-26 06:10:17 +0100

    fix: stop ignore db in src directory

---
+++ src/db/index.ts
@@ -0,0 +1,17 @@
+import path from "node:path";
+import fs from "node:fs";
+import Database from "better-sqlite3";
+import { schemaSql } from "./schema";
+
+const DB_DIR = path.join(process.cwd(), "db");
+const DB_PATH = path.join(DB_DIR, "bot.db");
+
+if (!fs.existsSync(DB_DIR)) {
+    fs.mkdirSync(DB_DIR, { recursive: true });
+}
+
+const db = new Database(DB_PATH);
+
+db.exec(schemaSql);
+
+export default db;
