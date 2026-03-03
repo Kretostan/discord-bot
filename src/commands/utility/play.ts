d27ebe3854e2d4aa4cee84e688962447c57a0d7d
Kretostan <kubakret82@icloud.com>
2026-03-03 19:07:45 +0100

    feat: add YouTube extractor

--- src/commands/utility/play.ts
+++ src/commands/utility/play.ts
@@ -1,10 +1,5 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction, MessageFlags, GuildMember } from "discord.js";
 import { getPlayer } from "../../lib/player.js";
-import play from "play-dl";
-
-function isYouTubeUrl(query: string) {
-  return /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\//i.test(query);
-}
 
 export default {
   data: new SlashCommandBuilder()
@@ -16,7 +11,7 @@ export default {
 
     const member = interaction.member;
     if (!member || !("voice" in member)) {
-      await interaction.reply({
+      return interaction.reply({
         content: "This command can only be used in guild.",
         flags: MessageFlags.Ephemeral,
       });
@@ -33,11 +28,6 @@ export default {
 
     let query = interaction.options.getString("query", true);
     await interaction.deferReply();
-
-    if (isYouTubeUrl(query)) {
-      const info = await play.video_basic_info(query);
-      console.log(`\nGram z YouTube: ${info.video_details.title}\n`);
-    }
 
     try {
       const { track } = await player.play(channel as any, query);
