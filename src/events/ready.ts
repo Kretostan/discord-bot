30f1303b0936ff560d41fc31013648007ff40f46
Kretostan <kubakret82@icloud.com>
2026-03-03 18:11:53 +0100

    feat: init music player after client initialization

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,13 +1,16 @@
 import { type Client, Events } from "discord.js";
+import { initPlayer } from "../lib/player.js";
 
 export default {
-	name: Events.ClientReady,
-	once: true,
-	execute(client: Client) {
-		if (!client.user) {
-			console.log("Logging in failed");
-			return;
-		}
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
+  name: Events.ClientReady,
+  once: true,
+  async execute(client: Client) {
+    if (!client.user) {
+      console.log("Logging in failed");
+      return;
+    }
+    console.log(`Ready! Logged in as ${client.user.tag}`);
+    await initPlayer(client);
+    console.log("Music player initialized, and extractors loaded.")
+  },
 };
