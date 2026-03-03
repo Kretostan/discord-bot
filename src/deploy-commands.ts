6d808f9683a770b77ece53e1a2406d10acb28769
Kretostan <kubakret82@icloud.com>
2026-03-03 18:12:38 +0100

    fix: add .js extension to dotenv import

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
-import "./utils/dotenv";
+import "./utils/dotenv.js";
 import { REST, Routes } from "discord.js";
 
 const __dirname = import.meta.dirname;
